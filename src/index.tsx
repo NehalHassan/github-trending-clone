90c6f7edf7d5cc5eda01ea2168c4e0ce6bfda556
NehalHassan <nh.elsaify@gmail.com>
2021-10-07 17:57:04 +0200

    reconstruct project index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import styled from "@emotion/styled";
+import { css, Global } from "@emotion/react";
+import { Trending } from "./trending";
+
+const Wrapper = styled.div({
+  display: "flex",
+  flexFlow: "column",
+  alignItems: "center",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Wrapper>
+        <Global
+          styles={css`
+            body {
+              font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+                Helvetica, Arial, sans-serif, "Apple Color Emoji",
+                "Segoe UI Emoji";
+              font-size: 14px;
+              line-height: 1.5;
+              color: #adbac7;
+              background-color: #22272e;
+            }
+          `}
+        />
+        <Switch>
+          <Route path="/trending">
+            <Trending />
+          </Route>
+          <Redirect from="/" to="/trending" />
+        </Switch>
+      </Wrapper>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
