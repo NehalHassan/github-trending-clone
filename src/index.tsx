44ee8581e70f14316170ba144153024aef0821f9
NehalHassan <nh.elsaify@gmail.com>
2021-10-11 02:36:56 +0200

    integrations with react query

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import styled from "@emotion/styled";
 import { css, Global } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { Trending } from "./trending";
+
+const queryClient = new QueryClient();
 
 const Wrapper = styled.div({
   display: "flex",
@@ -13,30 +18,33 @@ const Wrapper = styled.div({
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Wrapper>
-        <Global
-          styles={css`
-            body {
-              font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
-                Helvetica, Arial, sans-serif, "Apple Color Emoji",
-                "Segoe UI Emoji";
-              font-size: 14px;
-              line-height: 1.5;
-              margin: 0;
-              color: #adbac7;
-              background-color: #22272e;
-            }
-          `}
-        />
-        <Switch>
-          <Route path="/trending">
-            <Trending />
-          </Route>
-          <Redirect from="/" to="/trending" />
-        </Switch>
-      </Wrapper>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Wrapper>
+          <Global
+            styles={css`
+              body {
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+                  Helvetica, Arial, sans-serif, "Apple Color Emoji",
+                  "Segoe UI Emoji";
+                font-size: 14px;
+                line-height: 1.5;
+                margin: 0;
+                color: #adbac7;
+                background-color: #22272e;
+              }
+            `}
+          />
+          <Switch>
+            <Route path="/trending">
+              <Trending />
+            </Route>
+            <Redirect from="/" to="/trending" />
+          </Switch>
+        </Wrapper>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
